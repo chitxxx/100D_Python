a0b91d73afe83f69aa5f9d5798d90d0129c3e3fc
chitxxx <hcchitxxx@gmail.com>
2022-09-13 23:17:08 +0100

    add tip calculator

---
+++ wk2/tip_calculator.py
@@ -0,0 +1,7 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill?"))
+tip = float(input("How much tip would you like to give? (%)"))
+people = int(input("How many people to split the bill?"))
+bill_with_tip = bill*(1+tip/100)
+per_person_bill = bill_with_tip/people
+print(f"Each person should pay ${round(per_person_bill,2)}.")
