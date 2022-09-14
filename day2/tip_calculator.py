8fa613a31dc01ae932ba8774a1ef8cdee6a9d7b3
chitxxx <hcchitxxx@gmail.com>
2022-09-14 23:21:57 +0100

    reformat code

--- day2/tip_calculator.py
+++ day2/tip_calculator.py
@@ -2,6 +2,6 @@ print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill?"))
 tip = float(input("How much tip would you like to give? (%)"))
 people = int(input("How many people to split the bill?"))
-bill_with_tip = bill*(1+tip/100)
-per_person_bill = bill_with_tip/people
-print(f"Each person should pay ${round(per_person_bill,2)}.")
+bill_with_tip = bill * (1 + tip / 100)
+per_person_bill = bill_with_tip / people
+print(f"Each person should pay ${round(per_person_bill, 2)}.")
