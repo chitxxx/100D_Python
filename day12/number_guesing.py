3edf8ce9e1a4d1533e1364303e841cec2d618c41
chitxxx <hcchitxxx@gmail.com>
2022-09-15 22:10:32 +0100

    functionalize game

--- day12/number_guesing.py
+++ day12/number_guesing.py
@@ -1,46 +1,45 @@
 import numpy as np
 
 BANNER = """
-                                                 ,--,                                                                                                        
-,-.----.                                      ,---.'|                                     ,----..                                                       ,--. 
-\    /  \                ,-.----.       ,---,.|   | :                           ,---,.   /   /   \  ,-.----.               ,---,.                     ,--.'| 
-|   :    \          ,--, \    /  \    ,'  .' |:   : |          ,---,          ,'  .' |  /   .     : \    /  \            ,'  .' |         ,--,    ,--,:  : | 
-|   |  .\ :       ,'_ /| ;   :    \ ,---.'   ||   ' :         /_ ./|        ,---.'   | .   /   ;.  \;   :    \         ,---.'   |       ,'_ /| ,`--.'`|  ' : 
-.   :  |: |  .--. |  | : |   | .\ : |   |   .';   ; '   ,---, |  ' :        |   |   .'.   ;   /  ` ;|   | .\ :         |   |   .'  .--. |  | : |   :  :  | | 
-|   |   \ :,'_ /| :  . | .   : |: | :   :  |-,'   | |__/___/ \.  : |        :   :  :  ;   |  ; \ ; |.   : |: |         :   :  :  ,'_ /| :  . | :   |   \ | : 
-|   : .   /|  ' | |  . . |   |  \ : :   |  ;/||   | :.'|.  \  \ ,' '        :   |  |-,|   :  | ; | '|   |  \ :         :   |  |-,|  ' | |  . . |   : '  '; | 
-;   | |`-' |  | ' |  | | |   : .  / |   :   .''   :    ; \  ;  `  ,'        |   :  ;/|.   |  ' ' ' :|   : .  /         |   :  ;/||  | ' |  | | '   ' ;.    ; 
-|   | ;    :  | | :  ' ; ;   | |  \ |   |  |-,|   |  ./   \  \    '         |   |   .''   ;  \; /  |;   | |  \         |   |   .':  | | :  ' ; |   | | \   | 
-:   ' |    |  ; ' |  | ' |   | ;\  \'   :  ;/|;   : ;      '  \   |         '   :  '   \   \  ',  / |   | ;\  \        '   :  '  |  ; ' |  | ' '   : |  ; .' 
-:   : :    :  | : ;  ; | :   ' | \.'|   |    \|   ,/        \  ;  ;         |   |  |    ;   :    /  :   ' | \.'        |   |  |  :  | : ;  ; | |   | '`--'   
-|   | :    '  :  `--'   \:   : :-'  |   :   .''---'          :  \  \        |   :  \     \   \ .'   :   : :-'          |   :  \  '  :  `--'   \'   : |       
-`---'.|    :  ,      .-./|   |.'    |   | ,'                  \  ' ;        |   | ,'      `---`     |   |.'            |   | ,'  :  ,      .-./;   |.'       
-  `---`     `--`----'    `---'      `----'                     `--`         `----'                  `---'              `----'     `--`----'    '---'         
-                                                                                                                                                           
+ _      _     _      ____  _____ ____  ____ 
+/ \  /|/ \ /\/ \__/|/  __\/  __//  __\/ ___\
+| |\ ||| | ||| |\/||| | //|  \  |  \/||    \
+| | \||| \_/|| |  ||| |_\\|  /_ |    /\___ |
+\_/  \|\____/\_/  \|\____/\____\\_/\_\\____/
+                                                                                                                             
  """
 
 LIFE_CONFIG = {'easy': 10,
                   'hard': 5}
+def set_lives():
+    level = input("Choose a difficulty. Type 'easy' or 'hard'.")
+    lifes = LIFE_CONFIG[level]
+    return lifes
 
-print(BANNER)
-print("Welcome to the game!")
-level = input("Choose a difficulty. Type 'easy' or 'hard'.")
-answer = np.random.randint(low = 1, high = 100)
-life = LIFE_CONFIG[level]
-
-while life > 0:
-    print(f'You have {life} lives.')
-    guess = int(input("Take a guess."))
-    if guess == answer:
-        break
-    elif guess > answer:
+def check_guess(guess, answer):
+    if guess > answer:
         print('Too high.')
-    else:
+    elif guess < answer:
         print('Too low.')
+    else:
+        print('You got it!')
+    return guess == answer
 
-    life -= 1
+def run_game():
+    print(BANNER)
+    print("Welcome to the game!")
 
-if life > 0:
-    print('You are right!')
-else:
-    print(f'You have {life} lives, you lost.')
+    lives = set_lives()
+    answer = np.random.randint(low = 1,high = 100)
+    while lives > 0:
+        print(f'You have {lives} lives.')
+        guess = int(input("Take a guess."))
+        correct = check_guess(guess, answer)
+        if correct:
+            return
+        lives -=1
+
+    else:
+        print(f'You have {lives} lives, you lost.')
+
+run_game()
