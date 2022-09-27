03efc51d51d24aa529c307dd0012f44f872d17cb
Henry Tse <hcchitxxx@gmail.com>
2022-09-27 14:32:18 +0100

    add random forward action

--- day19/turtle_race.py
+++ day19/turtle_race.py
@@ -1,5 +1,6 @@
 import turtle
 from turtle import Turtle, Screen
+import random
 
 ## set config
 N_TURTLES = 6
@@ -18,10 +19,22 @@ turtles = []
 for i in range(N_TURTLES):
     turtles.append(Turtle())
     turtles[i].shape("turtle")
+    turtles[i].name = COLORS[i]
     turtles[i].color(COLORS[i])
     turtles[i].penup()
     turtles[i].goto(x=-0.9*WIDTH/2 + 10,
                     y= -HEIGHT/2 + i*0.9*HEIGHT/N_TURTLES + 50)
-fastest_x = max([turtle.])
+
+FINISH_LINE = 0.9*WIDTH/2 - 10
+fastest_position = max([turtle.pos()[0] for turtle in turtles])
+print(fastest_position)
+
+while fastest_position < FINISH_LINE:
+    moving_turtle = random.choice(turtles)
+    moving_turtle.forward(10)
+    fastest_position = max([turtle.pos()[0] for turtle in turtles])
+
+winner = [turtle.name for turtle in turtles if turtle.pos()[0] == FINISH_LINE][0]
+print(f"The {winner} turtle won!")
 
 screen.exitonclick()
