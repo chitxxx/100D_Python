155cfbb5db34de0eded8b4cf9b3fe0487c4d3a03
Henry Tse <hcchitxxx@gmail.com>
2022-10-19 11:31:39 +0100

    init pomodoro

---
+++ day28/pomodoro/main.py
@@ -0,0 +1,29 @@
+from tkinter import (Tk, Canvas, PhotoImage)
+
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+
+background_img = PhotoImage(file="tomato.png")
+canvas = Canvas()
+canvas.create_image(100, 112, image=background_img)
+canvas.pack()
+
+window.mainloop()
