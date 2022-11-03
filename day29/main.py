cc4277f3665689d3cb0dbbefac85ae5c47fcd7f9
chitxxx <hcchitxxx@gmail.com>
2022-11-03 18:12:34 +0000

    add password generator

--- day29/main.py
+++ day29/main.py
@@ -5,8 +5,7 @@ from tkinter import (Tk,
                      Entry,
                      Button,
                      messagebox)
-
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -57,6 +56,40 @@ button_add.grid(row=5, column=2, columnspan=2)
 DEFAULT_USERNAME = "user"
 entry_website.focus()
 entry_username.insert(0, DEFAULT_USERNAME)
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+
+def generate_password():
+    password_list = []
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+        
+    entry_password.delete(0, 'end')
+    entry_password.insert(0, password)
+
+button_password.config(command=generate_password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
